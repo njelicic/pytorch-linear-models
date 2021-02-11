e2c88dd21dcd1e69badc57827210eaf93c9de820
njelicic <66822854+njelicic@users.noreply.github.com>
2021-02-11 15:23:59 +0100

    Update regression.py
    Added fit_intercept function

--- regression.py
+++ regression.py
@@ -1,16 +1,17 @@
-import torch
-import seaborn as sns
-
-
 class LinearRegression():
     """"My custom regression class"""   
-    def __init__(self, C=0.1,lr=1e-3, penalty=None, n_iter=10000):
+    def __init__(self, C=0.1,lr=1e-3, penalty=None, n_iter=10000, fit_intercept=True):
         self.C = C
         self.lr = lr
         self.history = []
         self.penalty = penalty 
         self.n_iter = n_iter
+        self.fit_intercept= fit_intercept
         return
+    
+    def add_intercept(self,x):
+        a = torch.ones(x.size()[0],1)
+        return torch.cat((x,a),axis=1)
     
     def linreg(self, x):
         """"Linear regression function"""  
@@ -40,6 +41,9 @@ class LinearRegression():
         x = self.cast_to_tensor(x)
         y = self.cast_to_tensor(y)
         
+        if self.fit_intercept:
+            x = self.add_intercept(x)
+        
         self.w = torch.randn(x.size()[1], requires_grad=True) #instantiate weights
         self.b = torch.randn(1, requires_grad=True)           #instantiate bias
         
@@ -59,6 +63,10 @@ class LinearRegression():
     def predict(self, x):
         """"Predict"""  
         x = self.cast_to_tensor(x)
+        
+        if self.fit_intercept:
+            x = self.add_intercept(x)
+        
         return self.linreg(x).detach().numpy()
     
 
